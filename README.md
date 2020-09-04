bf366d61517d656e855986f1bb67a1dd52399efa
Ediel Lopez <ediellopez2@gmail.com>
2020-09-04 10:41:13 -0700

    Version 1.1 of site_restock.py

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ The purpose of this project is to scrape the specified URL, determine the availa
 
 Future Iterations:
 - send the Excel spreadsheet to the client/user via email. [DONE/COMPLETED - 7/11/2020 - find_my_hoya.py]
-- send the results to the client/user via SMS with the help of Twilio. [DONE/COMPLETED - 7/5/2020 - hoya_twilio_sms.py] 
+- send the results to the client/user via SMS with the help of Twilio. [DONE/COMPLETED - 7/5/2020 - hoya_twilio_sms.py]
+- send the client/user an SMS notification when the website is down for a restock [In Progress - Version 1.1] 
--- site_restock.py
+++ site_restock.py
@@ -2,6 +2,7 @@ import variables                   # Stores my Google Email & Password
 import requests                    # Web Scraping
 from bs4 import BeautifulSoup      # Web Scraping
 from twilio.rest import Client     # Twilio
+import time
 
 # Documentation:
 # https://www.crummy.com/software/BeautifulSoup/bs4/doc/#find
@@ -10,29 +11,31 @@ def send_sms():
     client = Client(variables.TWILIO_ACCOUNT_SID, variables.TWILIO_AUTH_TOKEN)
 
     client.messages.create(
-        body="THIS IS A TEST! Hoya update in progress.",
+        body="Hoya update in progress.",
         from_=variables.number_twilio,
         to=variables.number_me
     )
     return
 
 if __name__ == "__main__":
-    # soup = BeautifulSoup(open("C:/Users/elope/Desktop/hoya_site_restock.html"), "html.parser")
+    while (True):
+        # soup = BeautifulSoup(open("C:/Users/elope/Desktop/hoya_site_restock.html"), "html.parser")
 
-    page = requests.get("https://gardinonursery.com/product-category/categories/hoyas/hoyas-full-list/")
-    soup = BeautifulSoup(page.text, 'html.parser')
+        page = requests.get("https://gardinonursery.com/product-category/categories/hoyas/hoyas-full-list/")
+        soup = BeautifulSoup(page.text, 'html.parser')
 
-    # If the website is currently down due to a restock that is in progress, then
-    # <div class="information"> ... </div> will be present in the html code. If the
-    # site is up an running, the find_div = None.
-    find_div = soup.find('div', class_='information')
+        # If the website is currently down due to a restock that is in progress, then
+        # <div class="information"> ... </div> will be present in the html code. If the
+        # site is up an running, the find_div = None.
+        find_div = soup.find('div', class_='information')
 
-    if (find_div != None):
-        # Hey, it looks like the website is currently down due to a restock. Let's
-        # try to find the signature message that is displayed on the website.
-        if (find_div.find(string="Hoya update in progress.") == 'Hoya update in progress.'):
-            send_sms()
-    else:
-        print("The website is up and running.")
+        if (find_div != None):
+            # Hey, it looks like the website is currently down due to a restock. Let's
+            # try to find the signature message that is displayed on the website.
+            if (find_div.find(string="Hoya update in progress.") == 'Hoya update in progress.'):
+                send_sms()
+        else:
+            print("The website is up and running.")
 
-
+        find_div = None
+        time.sleep(1800)    # 30 minutes
