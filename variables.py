dd7dac0b609bc3b3560f6c020934d738dc22132b
Ediel <elopez69@ucmerced.edu>
2021-04-09 19:28:32 -0700

    variables file

---
+++ variables.py
@@ -0,0 +1,10 @@
+EMAIL_USER = '' 
+EMAIL_TO_EDIEL = ''
+EMAIL_TO_SISTER = ''
+EMAIL_PASS = ''
+
+TWILIO_ACCOUNT_SID = ""
+TWILIO_AUTH_TOKEN = ""
+number_twilio = ""
+number_ediel = ""
+number_sister = ""
